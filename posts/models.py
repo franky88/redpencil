d5538249653449c0a7b6e920b07d411520bf4898
frank <ramosfp99@gmail.com>
2017-03-22 15:56:28 -0700

    requirements.txt

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField(null=True, blank=True)
+	image = models.ImageField(upload_to="post_images/%Y/%m/%d", null=True, blank=True)
+	def __unicode__(self):
+		return self.title
