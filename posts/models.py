5fac2bc8cc067225ff26f2d63b6c8b5eb1fd741c
frank <ramosfp99@gmail.com>
2017-03-23 16:08:23 -0700

    model updates

--- posts/models.py
+++ posts/models.py
@@ -7,5 +7,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField(null=True, blank=True)
 	image = models.ImageField(upload_to="post_images/%Y/%m/%d", null=True, blank=True)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	def __unicode__(self):
 		return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def postIndex(request):
+	post_list = models.ForiegnKey(Post, on_delete=models.CASCADE)
