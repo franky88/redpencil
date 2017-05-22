ed463bb2e200031acc7db36db296d3824fa7dcbc
Franklin Ramos <ramosfp99@gmail.com>
2017-05-22 23:36:59 +0800

    update user link

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "posts"
 urlpatterns = [
     url(r'^$', views.postIndex, name="index"),
+    url(r'^addpost/$', views.addPost, name="addpost"),
     url(r'^details/(?P<pk>[0-9]+)/$', views.postDetail, name="detail"),
     url(r'^category/(?P<pk>[0-9]+)/$', views.postCatDetail, name="catdetail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, Http404
+from django.contrib.auth.models import User
 from .models import Post, Category
+from .forms import PostForm
 # Create your views here.
 def postIndex(request):
 	cat_list = Category.objects.all()
@@ -28,3 +30,25 @@ def postCatDetail(request, pk):
 		"catinstance": cat_instance,
 	}
 	return render(request, "posts/posts_category_detail.html", context)
+
+def addPost(request):
+	if not request.user.is_authenticated():
+		raise Http404
+
+	if request.method == 'POST':
+		form = PostForm(
+			request.POST or None, 
+			request.FILES or None,
+			)
+		if form.is_valid():
+			instance = form.save(commit=False)
+			instance.user = request.user
+			instance.save()
+			return redirect('posts:detail', pk=instance.pk)
+	else:
+		form = PostForm()
+	context = {
+		"form": form,
+		"title": "add post",
+	}
+	return render(request, "posts/add_post.html", context)
